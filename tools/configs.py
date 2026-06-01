4ab6f52b58f18769fc002966b7ccf01cd95bfd65
Charmaine Chia <llamalitter@gmail.com>
2026-06-01 18:03:33 +0800

    modifies configs

--- tools/configs.py
+++ tools/configs.py
@@ -6,20 +6,20 @@ from variables import address_dict
 
 
 YASARA_CONFIG = {
-    'version_suffix': '2025',
+    'version_suffix': '2026',
 }
 
 
 DATA_CONFIG = {
     'data_folder': address_dict['influenza-resistance'],
     'data_subfolder': '',
-    'inputs': 'PA-NA_benchmark_missed.csv',
+    'inputs': 'PA-NA_benchmark.csv',
     'output_fname': f'PA-NA_benchmark_yasara{YASARA_CONFIG["version_suffix"]}',
 }
 
 
 RUN_CONFIG = {
-    'run_multiprocessing': 8,
+    'run_multiprocessing': 16,
     'save_minimized_struct': True,
     'fix_metal_ion': True,
     'append_to_existing_output': True,
